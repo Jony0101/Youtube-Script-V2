983579593685c9919fcdb15783222ad01a106221
Jony0101 <121823452+Jony0101@users.noreply.github.com>
2024-01-30 23:36:32 +0000

    Add files via upload

---
+++ Files/youtubeadsuser.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Youtube Ads Skipper
+// @namespace    https://www.youtube.com/
+// @version      2.0
+// @description  Automatically Skips Youtube Ads
+// @author       Jony
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+
+// @require https://github.com/Jony0101/Youtube-Script-V2/raw/main/Files/youtubeadsmin.js
+// @downloadURL https://github.com/Jony0101/Youtube-Script-V2/raw/main/Files/youtubeadsmin.js
+// @updateURL https://github.com/Jony0101/Youtube-Script-V2/raw/main/Files/youtubeadsmin.js
+
+// ==/UserScript==
+
+//Automatically Skips youtube ads :)
